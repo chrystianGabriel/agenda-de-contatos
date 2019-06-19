57fc804cf232dbc4e20555962693d7780a08efc3
Lucas <lucas.anterocf@hotmail.com>
2019-06-19 01:02:35 -0300

    Adapter contatos

--- app/src/main/java/com/example/agendadecontatos/MainActivity.kt
+++ app/src/main/java/com/example/agendadecontatos/MainActivity.kt
@@ -4,12 +4,30 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var c1 = Contatos()
+        c1.nome = "Teste 1"
+        c1.telefone = "Teste tel 1"
+        c1.email = "Test email 1"
+        var c2 = Contatos()
+        c1.nome = "Teste 2"
+        c1.telefone = "Teste tel 2"
+        c1.email = "Test email 2"
+        val dados = ArrayList<Contatos>()
+        dados.add(c1)
+        dados.add(c2)
+
+        val list : ListView = findViewById(R.id.list_contatos) as ListView
+        val adapter = ListViewAdapater(this, R.layout.layout_contato, dados)
+        list.adapter = adapter
     }
 
     fun click_adicionar(view : View){
